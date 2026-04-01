4cfe3d2ec9841d51092c161d748f31608fa3867d
picturecube <152343360+picturecube@users.noreply.github.com>
2026-04-01 15:59:19 -0400

    Delete jets-io directory

--- jets-io/form-submit.js
+++
@@ -1,8 +0,0 @@
-usernameForm = $('#player-name-form');
-
-usernameForm.on('submit', function(event) {
-    event.preventDefault();
-    const playerName = $('#player-name-input').val();
-    document.cookie = "playerName=" + playerName;
-    window.location.href = "game.html";
-});
